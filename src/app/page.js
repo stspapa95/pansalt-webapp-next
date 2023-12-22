561c229c9c18942b34437242f359a2628336da68
stspapa95 <sts.papa@gmail.com>
2023-12-22 17:44:12 +0200

    Further clean-up + modified navbar

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export const metadata = {
       "https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap",
       "https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap",
       "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap",
-      "/css/dark.css",
+      "/css/light.css",
     ]),
   },
 };
--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useEffect, useRef } from "react";
 import Link from "next/link";
 //= Scripts
@@ -24,7 +24,12 @@ const Navbar = ({ lr, theme }) => {
   });
 
   return (
-    <nav ref={navbar} className={`navbar navbar-expand-lg change ${theme === "light" ? "light" : ""}`}>
+    <nav
+      ref={navbar}
+      className={`navbar navbar-expand-lg change ${
+        theme === "light" ? "light" : ""
+      }`}
+    >
       <div className="container">
         <Link className="logo" href="/">
           {theme ? (
@@ -55,79 +60,35 @@ const Navbar = ({ lr, theme }) => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Home
-              </span>
-              <div className="dropdown-menu">
-                <a className="dropdown-item" href={`/homepage/home1-dark`}>
-                  Main Home
-                </a>
-                <a className="dropdown-item" href={`/homepage/home2-dark`}>
-                  Creative Agency
-                </a>
-                <a className="dropdown-item" href={`/homepage/home5-dark`}>
-                  Digital Agency
-                </a>
-                <a className="dropdown-item" href={`/homepage/home4-dark`}>
-                  Business One Page
-                </a>
-                <a className="dropdown-item" href={`/homepage/home3-dark`}>
-                  Corporate Business
-                </a>
-                <a className="dropdown-item" href={`/homepage/home6-dark`}>
-                  Modern Agency
-                </a>
-                <a className="dropdown-item" href={`/homepage/home7-dark`}>
-                  Freelancer
-                </a>
-                <a className="dropdown-item" href={`/homepage/home8-dark`}>
-                  Architecture
-                </a>
-              </div>
-            </li>
             <li className="nav-item">
-              <a className="nav-link" href={`/about/about-dark`}>
+              <a className="nav-link" href={`/about`}>
                 About
               </a>
             </li>
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Works
-              </span>
-              <div className="dropdown-menu">
-                <a className="dropdown-item" href={`/showcase/showcase-dark`}>
-                  Showcase Parallax
-                </a>
-                <a className="dropdown-item" href={`/showcase4/showcase4-dark`}>
-                  Showcase Carousel
-                </a>
-                <a className="dropdown-item" href={`/showcase3/showcase3-dark`}>
-                  Showcase Circle
-                </a>
-                <a className="dropdown-item" href={`/works/works-dark`}>
-                  Portfolio Masonry
-                </a>
-                <a className="dropdown-item" href={`/works2/works2-dark`}>
-                  Portfolio Filtering
-                </a>
-                <a className="dropdown-item" href={`/works3/works3-dark`}>
-                  Portfolio Gallery
-                </a>
-              </div>
-            </li>
+
+            {/*<li className="nav-item dropdown" onClick={handleDropdown}>*/}
+            {/*  <span*/}
+            {/*    className="nav-link dropdown-toggle"*/}
+            {/*    data-toggle="dropdown"*/}
+            {/*    role="button"*/}
+            {/*    aria-haspopup="true"*/}
+            {/*    aria-expanded="false"*/}
+            {/*  >*/}
+            {/*    Works*/}
+            {/*  </span>*/}
+            {/*  <div className="dropdown-menu">*/}
+            {/*    <a className="dropdown-item" href={`/works/works-dark`}>*/}
+            {/*      Portfolio Masonry*/}
+            {/*    </a>*/}
+            {/*    <a className="dropdown-item" href={`/works2/works2-dark`}>*/}
+            {/*      Portfolio Filtering*/}
+            {/*    </a>*/}
+            {/*    <a className="dropdown-item" href={`/works3/works3-dark`}>*/}
+            {/*      Portfolio Gallery*/}
+            {/*    </a>*/}
+            {/*  </div>*/}
+            {/*</li>*/}
+
             <li className="nav-item dropdown" onClick={handleDropdown}>
               <span
                 className="nav-link dropdown-toggle"
@@ -139,22 +100,26 @@ const Navbar = ({ lr, theme }) => {
                 Blog
               </span>
               <div className="dropdown-menu">
-                <a className="dropdown-item" href={`/blog/blog-dark`}>
-                  Blog Standerd
+                <a className="dropdown-item" href={`/blog`}>
+                  Blog Standard
                 </a>
-                <a className="dropdown-item" href={`/blog-list/blog-list-dark`}>
+                <a className="dropdown-item" href={`/blog-list`}>
                   Blog List
                 </a>
-                <a className="dropdown-item" href={`/blog-grid/blog-grid-dark`}>
+                <a className="dropdown-item" href={`/blog-grid`}>
                   Blog Grid
                 </a>
-                <a className="dropdown-item" href={`/blog-details/blog-details-dark`}>
+                <a
+                  className="dropdown-item"
+                  href={`/blog-details`}
+                >
                   Blog Details
                 </a>
               </div>
             </li>
+
             <li className="nav-item">
-              <a href={`/contact/contact-dark`} className="nav-link">
+              <a href={`/contact`} className="nav-link">
                 Contact
               </a>
             </li>
